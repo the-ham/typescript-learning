6099f8cccfd2f5f6c7af63db2a945bb98dec8cf8
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-25 19:22:45 +1100

    hello world

---
+++ helloWorld.js
@@ -0,0 +1,4 @@
+var myName = "Ambery";
+var myNum = 22;
+console.log(myNum);
+console.log(myName);
---
+++ helloWorld.ts
@@ -0,0 +1,5 @@
+const myName: string = "Ambery"
+const myNum: number = 22
+
+console.log(myNum);
+console.log(myName);
