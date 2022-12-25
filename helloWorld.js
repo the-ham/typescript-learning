d88a7af31a4fe09953d888e6bb5b35783c3f448d
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-25 20:39:57 +1100

    1. Declaring typed variables 2. type "any" and how to avoid it 3. Declaring functions and specifying both the parameter and output types

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,33 @@
+"use strict";
+exports.__esModule = true;
+// Hello world example and declaring typed variables
 var myName = "Ambery";
 var myNum = 22;
-console.log(myNum);
+/*
+actually don't have to explicitly type the variables if it is straightforward
+- this is called "type inference"
+*/
+var isAlive = true;
 console.log(myName);
+/////////////////////////////////////////////////////////////////////
+// The "any" type, how it tends to manifest and that it SHOULD NOT BE ALLOWED to exist!
+var variable; // change this line to "let variable: string;" to restrict future references to type string
+var someFunc = function () {
+    return "a string!";
+};
+variable = someFunc(); // Here if you hover "variable" it shows that it is of type "any", BAD
+/////////////////////////////////////////////////////////////////////
+// How to write a function accepting typed paramenters
+// regular functions:
+function userSignUp(name, email, isPremium) {
+    return name.toUpperCase;
+}
+userSignUp(1, 2, 3); // Typescript throws error - good!
+// For a function that both accepts AND outputs a specified type:
+function getAPos(s) {
+    return s.toLowerCase().search("a");
+}
+// In arrow function form:
+var getBPos = function (s) {
+    return s.toLowerCase().search("b");
+};
--- helloWorld.ts
+++ helloWorld.ts
@@ -1,5 +1,45 @@
-const myName: string = "Ambery"
-const myNum: number = 22
+// Hello world example and declaring typed variables
+const myName: string = "Ambery";
+const myNum: number = 22;
 
-console.log(myNum);
+/*
+actually don't have to explicitly type the variables if it is straightforward
+- this is called "type inference"
+*/
+const isAlive = true;
+
 console.log(myName);
+
+/////////////////////////////////////////////////////////////////////
+// The "any" type, how it tends to manifest and that it SHOULD NOT BE ALLOWED to exist!
+
+let variable; // change this line to "let variable: string;" to restrict future references to type string
+
+const someFunc = () => {
+  return "a string!"
+}
+
+variable = someFunc(); // Here if you hover "variable" it shows that it is of type "any", BAD
+
+/////////////////////////////////////////////////////////////////////
+// How to write a function accepting typed paramenters
+// regular functions:
+function userSignUp(name: string, email: string, isPremium: boolean) {
+  return name.toUpperCase;
+}
+
+userSignUp(1,2,3); // Typescript throws error - good!
+
+// For a function that both accepts AND outputs a specified type:
+function getAPos(s: string): number {
+  return s.toLowerCase().search("a");
+}
+
+// In arrow function form:
+const getBPos = (s: string): number => {
+  return s.toLowerCase().search("b")
+}
+
+/////////////////////////////////////////////////////////////////////
+
+export {}
